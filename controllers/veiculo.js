e67e5dfcc260ba0ec79e765c863a64fb9bde73fd
jeffersonarar <jeffersonarar@hotmail.com>
2015-09-17 14:14:14 -0300

    Removido o req.params e colocado o req.query; Mudança na rota;

--- controllers/veiculo.js
+++ controllers/veiculo.js
@@ -5,15 +5,20 @@ module.exports = function(app) {
     var veiculoController = {
         
         procurarVeiculo: function(req, res) {
-             console.dir(req.params);
+             console.dir(req.query);
             Veiculo.procurarVeiculo(
-                req.params.placa)
+                req.query.placa)
+
             .then(function(result) {
-                    console.dir(result);
-                    res.send(result.toJSON());
+                    console.dir(result)
+                    if(result){
+                        res.send(result.toJSON())
+                    }else{
+                        res.status(404);
+                    }
                 }).catch(function(err) {
 
-                    res.status(404).send('' + result);
+                    res.status(404);
                     console.dir(err);
                     return err;
                 });
--- routes/veiculo.js
+++ routes/veiculo.js
@@ -2,5 +2,5 @@ module.exports = function(app){
 	var veiculo = app.controllers.veiculo;
 	var routesUtil = require('../routes/utils');
 
-	app.get("/veiculo?placa=:placa", veiculo.procurarVeiculo);
+	app.get("/veiculo", veiculo.procurarVeiculo);
 }
