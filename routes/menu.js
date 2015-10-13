7e1189a9f977a11549dd2bf79de2db7c6ff54c45
jeffersonarar <jeffersonarar@hotmail.com>
2015-10-13 11:22:41 -0300

    Excluido arquivo em desuso

--- routes/menu.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-	var menu = app.controllers.menu;
-	 var routesUtil = require('../routes/utils');
-
-	app.get("/",  routesUtil.ensureAuthenticated, menu.index);
-	app.get("/menu_cadastro",  routesUtil.ensureAuthenticated, menu.menu_cadastro);
-}
