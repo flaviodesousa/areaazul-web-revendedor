7221d6da79930bbeeb5175fd59bc5091019b1e31
Willian Brener <willian_babao@hotmail.com>
2015-11-13 13:22:11 -0200

    - Teste commit

--- controllers/ativacao.js
+++ controllers/ativacao.js
@@ -60,7 +60,6 @@ module.exports = function(app) {
                 tipo_veiculo: req.body.tipo_veiculo,
                 usuario_pessoa_id: req.session.passport.user,
             };
-            console.log("dadosAtivacao:"+dadosAtivacao);
             Ativacao.ativarPelaRevenda(dadosAtivacao)
                 .then(function(revenda) {
                     req.flash('info', 'Veiculo com a placa ' + req.body.placa + 'foi ativado');
