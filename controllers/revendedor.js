7aafcfef061882e81db0bb1cb0b1d8be7ec308c6
Flavio de Sousa <fl@viodesousa.com>
2016-09-10 18:04:23 +0200

    reformat

--- controllers/revendedor.js
+++ controllers/revendedor.js
@@ -1,82 +1,72 @@
-module.exports = function(app) {
-    var AreaAzul = require('areaazul');
-    var Revendedor = AreaAzul.db.model('Revendedor');
-    var Ativacao = AreaAzul.db.model('Ativacao');
-    var Veiculo = AreaAzul.db.model('Veiculo');
+module.exports = function () {
+  const debug = require('debug')('areaazul-web-revendedor:controllers:revendedor');
+  var AreaAzul = require('areaazul');
+  var Revendedor = AreaAzul.db.model('Revendedor');
+  var Ativacao = AreaAzul.db.model('Ativacao');
+  var Veiculo = AreaAzul.db.model('Veiculo');
 
+  return {
+    index: function (req, res) {
+      res.render('revendedor/cadastro', {values: req.body});
+    },
 
-    var revendedorController = {
-        index: function(req, res) {
-            res.render("revendedor/cadastro", {values: req.body});
-        },
+    cadastrar: function (req, res) {
+      var parametros = null;
+      if (req.body.hiddenFormPJ === '1') {
+        parametros = {
+          cnpj: req.body.cnpj,
+          login: req.body.nome_usuario_pj,
+          email: req.body.email_responsavel_pj,
+          nova_senha: req.body.nova_senha_pj,
+          conf_senha: req.body.conf_senha_pj,
+          telefone: req.body.celular_pj,
+          nome_fantasia: req.body.nome_fantasia_pj,
+          razao_social: req.body.razao_social_pj,
+          cpf: req.body.cpf_responsavel_pj,
+          nome: req.body.responsavel_nome_pj,
+          autorizacao: 'administrador',
+          termo_servico: req.body.termo_servico,
+        }
+      } else {
+        parametros = {
+          cpf: req.body.cpf,
+          login: req.body.nome_usuario_pf,
+          email: req.body.email_pf,
+          nova_senha: req.body.senha_pf,
+          conf_senha: req.body.confirmar_senha_pf,
+          telefone: req.body.celular_pf,
+          nome: req.body.nome_pf,
+          autorizacao: 'administrador',
+          termo_servico: req.body.termo_servico,
+        }
+      }
 
-        pessoafisica: function(req, res) {
-            res.render("revendedor/indexpf", {values: req.body});
-        },
+      debug('Revendedor.cadastrar() parametros', parametros);
 
-        pessoajuridica: function(req, res) {
-            res.render("revendedor/indexpj", {values: req.body});
-        },
-        cadastrar: function(req, res) {
-            var parametros = null;
-            if (req.body.hiddenFormPJ === '1') {
-                var parametros = {
-                    cnpj: req.body.cnpj,
-                    login: req.body.nome_usuario_pj,
-                    email: req.body.email_responsavel_pj,
-                    senha: req.body.senha_pj,
-                    confirmar_senha: req.body.confirmar_senha_pj,
-                    celular: req.body.celular_pj,
-                    nome_fantasia: req.body.nome_fantasia_pj,
-                    razao_social: req.body.razao_social_pj,
-                    telefone: req.body.telefone_pj,
-                    cpf: req.body.cpf_responsavel_pj,
-                    nome: req.body.responsavel_nome_pj,
-                    autorizacao: 'administrador',
-                    termo_servico: req.body.termo_servico,
-                }
-            } else {
-                var parametros = {
-                    cpf: req.body.cpf,
-                    login: req.body.nome_usuario_pf,
-                    email: req.body.email_pf,
-                    senha: req.body.senha_pf,
-                    confirmar_senha: req.body.confirmar_senha_pf,
-                    telefone: req.body.celular_pf,
-                    nome: req.body.nome_pf,
-                    autorizacao: 'administrador',
-                    termo_servico: req.body.termo_servico,
-                }
+      Revendedor.cadastrar(
+        parametros)
+        .then(function () {
+          req.body = [];
+          req.flash('info', 'Salvo com sucesso!');
+          res.render('login/index', {message: req.flash('info')});
+        })
+        .catch(function (err) {
+          if (err.details) {
+            for (var i = 0; i < err.details.length; i++) {
+              req.flash('info', err.details[i].problem);
+              res.render('revendedor/cadastro', {
+                message: req.flash('info')
+                , values: req.body
+              });
             }
-
-            console.log('parametros');
-            console.dir(parametros);
-
-            Revendedor.cadastrar(
-                    parametros)
-                    .then(function(revenda) {
-
-                        req.body = [];
-                        req.flash('info', 'Salvo com sucesso!');
-                        res.render('login/index', {message: req.flash('info')});
-                    })
-                    .catch(function(err) {
-                        if (err.details) {
-
-                            for (var i = 0; i < err.details.length; i++) {
-                                req.flash('info', err.details[i].problem);
-                                res.render("revendedor/cadastro", {
-                                    message: req.flash('info')
-                                , values: req.body});
-                            }
-                        } else {
-                            req.flash('info', err);
-                            res.render("revendedor/cadastro", {
-                                message: req.flash('info')
-                            , values: req.body});
-                        }
+          } else {
+            req.flash('info', err);
+            res.render('revendedor/cadastro', {
+              message: req.flash('info')
+              , values: req.body
             });
-        },
+          }
+        });
     }
-    return revendedorController;
-}
+  };
+};
--- public/js/revendedor/cadastro.js
+++ public/js/revendedor/cadastro.js
@@ -1,54 +1,54 @@
 function ver(elemento) {
-    if (elemento.checked) {
-        document.getElementById("formularioPessoaFisica").style.display = "";
-    } else {
-        document.getElementById("formularioPessoaFisica").style.display = "none";
-    }
+  if (elemento.checked) {
+    document.getElementById('formularioPessoaFisica').style.display = '';
+  } else {
+    document.getElementById('formularioPessoaFisica').style.display = 'none';
+  }
 }
 
 function verCampo(elemento) {
-    if (elemento.checked) {
-        document.getElementById("formularioPessoaJuridica").style.display = "";
-    } else {
-        document.getElementById("formularioPessoaJuridica").style.display = "none";
-    }
+  if (elemento.checked) {
+    document.getElementById('formularioPessoaJuridica').style.display = '';
+  } else {
+    document.getElementById('formularioPessoaJuridica').style.display = 'none';
+  }
 }
 
 function verPorRadio(elemento) {
-    switch (elemento) {
-        case "1":
-            document.getElementById('formularioPessoaFisica').style.display = "";
-            document.getElementById('formularioPessoaJuridica').style.display = "none";
-            document.getElementById('radioButtonPessoaFisica').checked = true;
-            document.getElementById('radioButtonPessoaJuridica').checked = false;
-            break;
-        case "2":
-            document.getElementById('formularioPessoaFisica').style.display = "none";
-            document.getElementById('formularioPessoaJuridica').style.display = "";
-            document.getElementById('radioButtonPessoaFisica').checked = false;
-            document.getElementById('radioButtonPessoaJuridica').checked = true;
-            break;
-    }
+  switch (elemento) {
+    case 'pf':
+      document.getElementById('formularioPessoaFisica').style.display = '';
+      document.getElementById('formularioPessoaJuridica').style.display = 'none';
+      document.getElementById('radioButtonPessoaFisica').checked = true;
+      document.getElementById('radioButtonPessoaJuridica').checked = false;
+      break;
+    case 'pj':
+      document.getElementById('formularioPessoaFisica').style.display = 'none';
+      document.getElementById('formularioPessoaJuridica').style.display = '';
+      document.getElementById('radioButtonPessoaFisica').checked = false;
+      document.getElementById('radioButtonPessoaJuridica').checked = true;
+      break;
+  }
 }
 
 function valorInicial() {
-    if (radioButtonPessoaFisica.checked) {
-        document.getElementById('formularioPessoaFisica').style.display = "";
-        document.getElementById('formularioPessoaJuridica').style.display = "none";
-    }
-    if (radioButtonPessoaJuridica.checked) {
-        document.getElementById('formularioPessoaFisica').style.display = "none";
-        document.getElementById('formularioPessoaJuridica').style.display = "";
-    }
+  if (radioButtonPessoaFisica.checked) {
+    document.getElementById('formularioPessoaFisica').style.display = '';
+    document.getElementById('formularioPessoaJuridica').style.display = 'none';
+  }
+  if (radioButtonPessoaJuridica.checked) {
+    document.getElementById('formularioPessoaFisica').style.display = 'none';
+    document.getElementById('formularioPessoaJuridica').style.display = '';
+  }
 }
 
 
-$(document).ready(function(){
-    $("#cpf_PessoaFisica").mask("999.999.999-99");
-    $("#celular_PessoaFisica").mask("(99) 9999-9999");
-    $("#cnpj_PessoaJuridica").mask("99.999.999/9999-99");
-    $("#telefone_PessoaJuridica").mask("(99) 9999-9999");
-    $("#cpf_PessoaJuridica").mask("999.999.999-99");
+$(document).ready(function () {
+  $('#cpf_PessoaFisica').mask('999.999.999-99');
+  $('#celular_PessoaFisica').mask('(99) 99999-9999');
+  $('#cnpj_PessoaJuridica').mask('99.999.999/9999-99');
+  $('#telefone_PessoaJuridica').mask('(99) 99999-9999');
+  $('#cpf_PessoaJuridica').mask('999.999.999-99');
 });
 
 /*Teste*/
