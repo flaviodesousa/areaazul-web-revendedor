3695554343cdef5fe30ca111a127ad92ae15c2be
Flavio de Sousa <fl@viodesousa.com>
2016-09-23 22:47:21 +0200

    corrigida variável de ambiente para endpoint da API

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-  res.set('X-AreaAzul-API-Endpoint', process.env.AREAAZUL_API_SERVER);
+  res.set('X-AreaAzul-API-Endpoint', process.env.AREAAZUL_API_ENDPOINT);
   next();
 });
 app.use(session({
