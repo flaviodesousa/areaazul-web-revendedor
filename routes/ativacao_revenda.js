3936dc4dc0ee05b58ab10c3fd055c1ccc7a1f21a
Flavio de Sousa <fl@viodesousa.com>
2016-10-02 17:58:04 +0200

    Dependências externas internalizadas

--- routes/ativacao_revenda.js
+++ routes/ativacao_revenda.js
@@ -1,9 +1,11 @@
 module.exports = function(app) {
-    var ativacao = app.controllers.ativacao;
-    var routesUtil = require('../routes/utils');
+  const ativacao = app.controllers.ativacao;
+  const routesUtil = require('../routes/utils');
 
-    app.get("/ativacao", routesUtil.ensureAuthenticated, ativacao.ativar);
-    app.post("/ativacao/salvar_ativacao", routesUtil.ensureAuthenticated, ativacao.salvarAtivacao);
-    app.get("/ativacao/cidades/:id", routesUtil.ensureAuthenticated, ativacao.listarCidades);
-    app.get("/ativacao/estados", routesUtil.ensureAuthenticated, ativacao.listarEstados);
-}
+  app.get('/ativacao',
+    routesUtil.ensureAuthenticated,
+    ativacao.ativar);
+  app.post('/ativacao/salvar_ativacao',
+    routesUtil.ensureAuthenticated,
+    ativacao.salvarAtivacao);
+};
