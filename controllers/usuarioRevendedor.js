105ed1edfaf3dd70193ded27d3551ef5d26c8b5f
Flavio de Sousa <fl@viodesousa.com>
2016-10-09 23:35:16 +0200

    Lista de usuários: obtendo lista a partir do id da revenda

--- controllers/usuarioRevendedor.js
+++ controllers/usuarioRevendedor.js
@@ -1,135 +1,126 @@
-module.exports = function(app) {
-    var AreaAzul = require('areaazul');
-    var Usuario_Revendedor = AreaAzul.db.model('UsuarioRevendedor');
-    var UsuariosRevendedores = AreaAzul.db.collection('UsuariosRevendedores');
-    var i = null;
+module.exports = function() {
+  const AreaAzul = require('areaazul');
+  const log = AreaAzul.log;
+  const UsuarioRevendedor = AreaAzul.db.model('UsuarioRevendedor');
+  const UsuariosRevendedores = AreaAzul.db.collection('UsuariosRevendedores');
 
-    var usuarioRevendedorController = {
-        index: function(req, res) {
+  return {
+    index: function(req, res) {
+      res.render('usuario_revendedor/cadastro', {
+        values: req.body
+      });
+    },
+    cadastrar: function(req, res) {
+      var parametros = {
+        cpf: req.body.cpf,
+        login: req.body.nome_usuario_pf,
+        email: req.body.email_pf,
+        senha: req.body.senha_pf,
+        confirmar_senha: req.body.confirmar_senha_pf,
+        telefone: req.body.celular_pf,
+        nome: req.body.nome_pf,
+        autorizacao: 'funcionario',
+        revendedor_id: req.session.passport.user,
+        termo_servico: req.body.termo_servico
+      };
+
+      UsuarioRevendedor.inserir(parametros)
+        .then(function() {
+          req.body = [];
+          req.flash('info', 'Salvo com sucesso!');
+          res.render('usuario_revendedor/cadastro', {
+            message: req.flash('info'),
+            values: req.body
+          });
+        })
+        .catch(AreaAzul.BusinessException, e => {
+          for (var i = 0; i < e.details.length; i++) {
+            req.flash('info', e.details[ i ].problem);
             res.render('usuario_revendedor/cadastro', {
-                values: req.body
+              message: req.flash('info'),
+              values: req.body
             });
-        },
-        cadastrar: function(req, res) {
-            var parametros = null;
+          }
+        })
+        .catch(function(err) {
+          log.err('Erro inserindo UsuarioRevendedor', err);
+          req.flash('info', err);
+          res.render('usuario_revendedor/cadastro', {
+            message: req.flash('info'),
+            values: req.body
+          });
+        });
+    },
+    listar: function(req, res) {
+      UsuariosRevendedores
+        .listarUsuarioRevenda(
+          req.user.get('revendedor_id'))
+        .then(function(listaDeUsusarios) {
+          res.render('usuario_revendedor/lista', {
+            lista: listaDeUsusarios.toJSON()
+          });
+        });
+    },
 
-            var parametros = {
-                cpf: req.body.cpf,
-                login: req.body.nome_usuario_pf,
-                email: req.body.email_pf,
-                senha: req.body.senha_pf,
-                confirmar_senha: req.body.confirmar_senha_pf,
-                telefone: req.body.celular_pf,
-                nome: req.body.nome_pf,
-                autorizacao: 'funcionario',
-                revendedor_id: req.session.passport.user,
-                termo_servico: req.body.termo_servico,
+    indexAlterar: function(req, res) {
+      res.render('usuario_revendedor/alterar');
+    },
+
+    alterarProcura: function(req, res) {
+      UsuarioRevendedor.procurar(req.params.id,
+        function(result) {
+          res.render('usuario_revendedor/alterar', {
+            value: result.attributes
+          });
+          return result;
+        });
+    },
+
+
+    alterarSalva: function(req, res) {
+      var parametros = {
+        cpf: req.body.cpf,
+        login: req.body.nome_usuario_pf,
+        email: req.body.email_pf,
+        senha: req.body.senha_pf,
+        confirmar_senha: req.body.confirmar_senha_pf,
+        nome: req.body.nome_pf,
+        telefone: req.body.telefone,
+        autorizacao: req.body.autorizacao,
+        revendedor_id: req.session.passport.user
+      };
+      UsuarioRevendedor.alterar(parametros)
+        .then(function() {
+          req.flash('info', 'Salvo com sucesso!');
+          res.redirect('/usuario_revendedor/lista');
+        })
+        .catch(function(err) {
+          if (err.details) {
+            for (var i = 0; i < err.details.length; i++) {
+              req.flash('info', err.details[ i ].problem);
+              res.redirect('usuario_revendedor/alterar');
             }
+          }
+        });
+    },
 
-                Usuario_Revendedor.inserir(parametros)
-                    .then(function(revenda) {
-                        req.body = [];
-                        req.flash('info', 'Salvo com sucesso!');
-                        res.render("usuario_revendedor/cadastro", {
-                            message: req.flash('info'),
-                            values: req.body
-                        });
-                    })
-                    .catch(function(err) {
-                        if (err.details) {
-                            for (var i = 0; i < err.details.length; i++) {
-                                req.flash('info', err.details[i].problem);
-                                res.render("usuario_revendedor/cadastro", {
-                                    message: req.flash('info'),
-                                    values: req.body
-                                });
-                            }
-                        } else {
-                            console.dir(err);
-                            req.flash('info', err);
-                            res.render("usuario_revendedor/cadastro", {
-                                message: req.flash('info'),
-                                values: req.body
-                            });
-                        }
-                    });
-        },
-        listar: function(req, res) {
-            UsuariosRevendedores
-              .listarUsuarioRevenda(req.session.passport.user)
-              .then(function (listaDeUsusarios) {
-                    res.render('usuario_revendedor/lista', {
-                        lista: listaDeUsusarios.toJSON()
-                    });
-                });
-        },
+    deletar: function(req, res) {
 
-        indexAlterar: function(req, res) {
-            res.render('usuario_revendedor/alterar');
-        },
+      UsuarioRevendedor
+        .desativar(req.params.id)
+        .then(
+          function(result) {
+            res.redirect('/usuario_revendedor/lista');
+            return result;
+          })
+        .catch(
+          function(result) {
 
-        alterarProcura: function(req, res) {
-            var autoriz = null;
-            Usuario_Revendedor.procurar(req.params.id,
-                function(result) {
-
-                    res.render('usuario_revendedor/alterar', {
-                        value: result.attributes
-                    });
-                    return result;
-                })
-        },
-
-
-        alterarSalva: function(req, res) {
-            var parametros = null;
-            var autorizacao = null;
-                console.dir(req.body);
-                console.log("--------------------------------------");
-            var parametros = {
-                cpf: req.body.cpf,
-                login: req.body.nome_usuario_pf,
-                email: req.body.email_pf,
-                senha: req.body.senha_pf,
-                confirmar_senha: req.body.confirmar_senha_pf,
-                nome: req.body.nome_pf,
-                telefone: req.body.telefone,
-                autorizacao: req.body.autorizacao,
-                revendedor_id: req.session.passport.user,
-            }
-            Usuario_Revendedor.alterar(parametros)
-                .then(
-                    function(result) {
-                        req.flash('info', 'Salvo com sucesso!');
-                        res.redirect("/usuario_revendedor/lista");
-                        message: req.flash('info')
-                    })
-                .catch(function(err) {
-                    if (err.details) {
-                        for (var i = 0; i < err.details.length; i++) {
-                            req.flash('info', err.details[i].problem);
-                            res.redirect('usuario_revendedor/alterar');
-                        }
-                    }
-                });
-        },
-
-        deletar: function(req, res) {
-
-            Usuario_Revendedor.desativar(req.params.id)
-                .then(
-                    function(result) {
-                        res.redirect("/usuario_revendedor/lista");
-                        return result;
-                    }).catch(
-                    function(result) {
-
-                        res.redirect("/usuario_revendedor/lista");
-                        return result;
-                    })
-
-        },
+            res.redirect('/usuario_revendedor/lista');
+            return result;
+          });
 
     }
-    return usuarioRevendedorController;
-}
+
+  };
+};
