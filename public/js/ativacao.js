47f1919787e53e430628ed16d5a14e25902d917c
Flavio de Sousa <fl@viodesousa.com>
2016-11-11 11:37:56 +0100

    Select2 components

--- public/js/ativacao.js
+++ public/js/ativacao.js
@@ -5,6 +5,7 @@ const AREAAZUL_API_HOST = Cookies.get('api-endpoint');
 $(document).ready(function() {
   const $campoPlaca = $('#campoPlaca');
   const $campoCelular = $('#campoCelular');
+  const $campoCidade = $('#campoCidade');
 
   // ------------MASCARAS------------------
   $campoPlaca.inputmask({
@@ -19,6 +20,29 @@ $(document).ready(function() {
     }
   });
   $campoCelular.inputmask('(99) 99999-9999');
+  $campoCidade.select2({
+    theme: 'bootstrap',
+    ajax: {
+      url: `${AREAAZUL_API_HOST}/cidade`,
+      dataType: 'json',
+      delay: 250,
+      data: function(params) {
+        return {
+          terms: params.term
+        };
+      },
+      processResults: function(data, params) {
+        // Parse the results into the format expected by Select2
+        // since we are using custom formatting functions we do not need to
+        // alter the remote JSON data, except to indicate that infinite
+        // scrolling can be used
+        return {
+          results: data.items
+        };
+      },
+      cache: true
+    }
+  });
 
   // ------------QUANDO OCORRER MUDANÇA NO CAMPO PLACA----------------
   $campoPlaca.change(function() {
