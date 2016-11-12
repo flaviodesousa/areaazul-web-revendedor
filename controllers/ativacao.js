f2e121adfeb398377ae4292a04993876746c5196
Flavio de Sousa <fl@viodesousa.com>
2016-11-12 22:37:57 +0100

    Ativacao: argumentos e métodos incorretos corrigidos

--- controllers/ativacao.js
+++ controllers/ativacao.js
@@ -1,56 +1,51 @@
 module.exports = function() {
   var AreaAzul = require('areaazul');
-  var Ativacao = AreaAzul.facade.Ativacao;
-  var Configuracao = AreaAzul.facade.Configuracao;
 
   return {
     ativar: function(req, res) {
-
-      var link = null;
-      if (req.user.autorizacao === 'administrador') {
-        link = 'ativacao/ativacao-por-administrador';
-      } else {
-        link = 'ativacao/ativacao-por-usuario';
-      }
-      res.render(link, { values: req.body });
+      AreaAzul.facade.Configuracao
+        .buscar()
+        .then(configuracao => {
+          var view = null;
+          if (req.user.autorizacao === 'administrador') {
+            view = 'ativacao/ativacao-por-administrador';
+          } else {
+            view = 'ativacao/ativacao-por-usuario';
+          }
+          var values = req.body;
+          values.cidade_id = configuracao.cidade_id;
+          res.render(view, { values: req.body, configuracao: configuracao });
+        });
     },
     salvarAtivacao: function(req, res) {
       var valor = 0;
-      var link = null;
+      var view = null;
       if (req.user.autorizacao === 'administrador') {
-        link = 'ativacao/ativacao-por-administrador';
+        view = 'ativacao/ativacao-por-administrador';
       } else {
-        link = 'ativacao/ativacao-por-usuario';
+        view = 'ativacao/ativacao-por-usuario';
       }
 
-      if (req.body.tempo !== null) {
-        var tamanhoArray = Configuracao.getConfiguracaoTempo().length;
-        var configuracoes = Configuracao.getConfiguracaoTempo();
-        for (var i = 0; i < tamanhoArray; i++) {
-          if (req.body.tempo === configuracoes[ i ].quantidade_tempo) {
-            valor = configuracoes[ i ].preco;
-          }
-        }
-      }
       var dadosAtivacao = {
         valor: valor,
         celular: req.body.celular,
-        cidade: 1,
+        cidade_id: req.body.cidade_id,
         tempo: req.body.tempo,
         marca: req.body.marca,
         modelo: req.body.modelo,
         cor: req.body.cor,
         placa: req.body.placa,
-        tipo_veiculo: req.body.tipo_veiculo,
-        usuario_pessoa_id: req.session.passport.user
+        tipo: req.body.tipo_veiculo,
+        usuario_revendedor_id: req.session.passport.user
       };
 
-      Ativacao.ativarPelaRevenda(dadosAtivacao)
+      AreaAzul.facade.Ativacao
+        .ativarPorRevenda(dadosAtivacao)
         .then(function(revenda) {
           req.body = [];
 
           req.flash('info', 'Ativação realizada com sucesso!');
-          res.render(link, {
+          res.render(view, {
             message: req.flash('info'),
             values: req.body
           });
@@ -61,14 +56,14 @@ module.exports = function() {
           if (err.details) {
             for (var i = 0; i < err.details.length; i++) {
               req.flash('info', err.details[ i ].problem);
-              res.render(link, {
+              res.render(view, {
                 message: req.flash('info'),
                 values: req.body
               });
             }
           } else {
             req.flash('info', err);
-            res.render(link, {
+            res.render(view, {
               message: req.flash('info'),
               values: req.body
             });
