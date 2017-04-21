363f5eaca0cd6a4dced026d45be1e66bc2669752
Flavio de Sousa <fl@viodesousa.com>
2017-04-21 19:01:20 +0200

    Porta configurada por variável de ambiente padronizada

--- bin/web-revendedor.js
+++ bin/web-revendedor.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var debug = require('debug')('areaazul');
-var app = require('../app');
+const debug = require('debug')('areaazul');
+const app = require('../app');
 
-app.set('port', process.env.PORT || 18360);
+app.set('port', process.env.AREAAZUL_WEB_REVENDEDOR || 18360);
 
-var server = app.listen(app.get('port'), 'localhost', function() {
+const server = app.listen(app.get('port'), 'localhost', function() {
   debug('Express server listening on port ' + server.address().port);
 });
